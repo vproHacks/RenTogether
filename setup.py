e1856da1187541a0be9e71a5a1e99e8d7f9a0ee2
Vraj Prajapati <vpro@Vrajs-MacBook-Air.local>
2021-01-16 22:37:18 -0500

    Fixed Dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as readme:
 
 setuptools.setup(
     name="RenTogether",
-    version='0.0.2',
+    version='0.0.3',
     author='Vraj Prajapati',
     author_email='vrajip@gmail.com',
     description='A Utility that Allows everyone to play VNs Together!',
