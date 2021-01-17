4e2a0066dc08dfccd713efc33968b2eaad073c90
Vraj Prajapati <vpro@Vrajs-MacBook-Air.local>
2021-01-16 23:14:17 -0500

    Patch Update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as readme:
 
 setuptools.setup(
     name="RenTogether",
-    version='0.0.5',
+    version='0.0.6',
     author='Vraj Prajapati',
     author_email='vrajip@gmail.com',
     description='A Utility that Allows everyone to play VNs Together!',
